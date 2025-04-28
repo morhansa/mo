0c17647a50a0ddbf27747fec520b472769b0f863
morhansa <moakamok@gmail.com>
2025-04-29 02:48:27 +0300

    Add files via upload

---
+++ PhoneMail/view/frontend/requirejs-config.js
@@ -0,0 +1,12 @@
+var config = {
+    config: {
+        mixins: {
+            'mage/validation': {
+                'MagoArab_PhoneMail/js/phone-validation': true
+            }
+        }
+    },
+    deps: [
+        'MagoArab_PhoneMail/js/checkout-fix'
+    ]
+};
